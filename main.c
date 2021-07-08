ea7d2be6a86dac871fcf4431e5477f17bd3931f8
murilo-oak <murilo63793@gmail.com>
2021-07-08 19:03:31 -0300

    feat: equal distribution of points to process

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ int main (int argc, char* argv[]) {
         n = USER_INPUT_MIN;
     }
 
-    int number_of_points = pow(10, n);
+    unsigned long number_of_points = 0;
 
     double buffer = 0;
     double pi = 0;
@@ -50,13 +50,16 @@ int main (int argc, char* argv[]) {
     srand(time(NULL) + process_rank); //unique seed for each process
 
     if(process_rank == SERVER_RANK) {
+        number_of_points = pow(10, n)/number_of_processes;
         clock_gettime(CLOCK_MONOTONIC, &timer.start);
     }
 
     // Server broadcast
-    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&number_of_points, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
 
     if (process_rank == SERVER_RANK) {
+        sum += monte_carlo_pi(number_of_points);
+        printf("PI %d: %lf\n", process_rank, sum);
         for (src_rank = 1; src_rank < number_of_processes; src_rank++) {
             msg_tag = 0;
             MPI_Recv(&buffer, 1, MPI_DOUBLE, src_rank, msg_tag, MPI_COMM_WORLD, &status);
@@ -66,12 +69,13 @@ int main (int argc, char* argv[]) {
     else { //clients
         msg_tag = 0;
         pi = monte_carlo_pi(number_of_points);
+        printf("PI %d: %lf\n", process_rank, pi);
         MPI_Send(&pi, 1, MPI_DOUBLE, SERVER_RANK, msg_tag, MPI_COMM_WORLD);
     }
         
     if(process_rank == SERVER_RANK) {
 
-        pi = sum / (number_of_processes - 1);
+        pi = sum / number_of_processes;
         
         clock_gettime(CLOCK_MONOTONIC, &timer.end);
 
